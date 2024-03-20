0dc841ae665f216ba129bcd203d07f022524e888
mrAibo <93589172+mrAibo@users.noreply.github.com>
2024-03-20 13:51:30 +0100

    Update multi_compress.py

--- multi_compress.py
+++ multi_compress.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Copyright (c) 2024 Aleksej Voronin
-# MIT License
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
